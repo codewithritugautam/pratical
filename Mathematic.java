8518f34c3bf6d878565b77539032c221c9628f09
codewithritugautam <122805714+codewithritugautam@users.noreply.github.com>
2023-01-20 21:51:57 +0530

    Add files via upload

---
+++ Mathematic.java
@@ -0,0 +1,11 @@
+class Subtraction
+{
+public static void main(String[]args)
+System.out.println("this first number is:");
+int a=5;
+int b=6;
+int sub=a-b;
+System.out.println(a);
+System.out.println("this second number is:");
+System.out.println("the sum is ="-sub);
+}}
