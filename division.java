05ba5363a7919c75ad668fe045dac965f14cdfc2
codewithritugautam <122805714+codewithritugautam@users.noreply.github.com>
2023-01-20 21:45:13 +0530

    Add files via upload

---
+++ division.java
@@ -0,0 +1,13 @@
+class division
+{
+public static void main(String args[])
+{
+int a=8;
+int b=3;
+int div=a/b;
+System.out.println(a);
+System.out.println(b);
+System.out.println("this first number is");
+System.out.println("this second number is");
+System.out.println("the div is ="+div);
+}}
