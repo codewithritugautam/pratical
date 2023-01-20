04894f4ac196989dda858a0ddfb77980d48d3dbb
codewithritugautam <122805714+codewithritugautam@users.noreply.github.com>
2023-01-20 21:52:24 +0530

    Add files via upload

---
+++ Rectangle.java
@@ -0,0 +1,13 @@
+class Rectangle
+{
+public static void main(String args[])
+{
+int l=5;
+int b=6;
+int area=l*b;
+int perameter=2*(l+b);
+System.out.println("the area of rectangle is:");
+System.out.println(area);
+System.out.println("the perameter of rectangle is:");
+System.out.println(perameter);
+}}
