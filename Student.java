a1eab5d95f538b548880ef3c1ef99c7464b96add
codewithritugautam <122805714+codewithritugautam@users.noreply.github.com>
2023-01-20 21:53:41 +0530

    Add files via upload

---
+++ Student.java
@@ -0,0 +1,18 @@
+class Student1
+{
+int slno;
+String name;
+}
+class Student{
+public static void main(String[]args)
+{
+Student1 s = new Student1();
+s.slno=01;
+s.name="ritu";
+System.out.println(s.slno);
+System.out.println(s.name);
+s.slno=02;
+s.name="pallabi";
+System.out.println(s.slno);
+System.out.println(s.name);
+}}
