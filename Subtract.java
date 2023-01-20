36e9fcc1641dc77cd88f493d37360817f453fa71
codewithritugautam <122805714+codewithritugautam@users.noreply.github.com>
2023-01-20 21:54:09 +0530

    Add files via upload

---
+++ Subtract.java
@@ -0,0 +1,12 @@
+class Subtract
+{
+public static void main(String args[])
+{
+int a=8;
+int b=5;
+int sub=a-b;
+System.out.println("first number is:");
+System.out.println("second number is:");
+System.out.println("the sum is="+sub);
+}}
+
