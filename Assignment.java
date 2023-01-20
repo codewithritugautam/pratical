8390998175ce72d1d36943d9c56f73a97a0d7c10
codewithritugautam <122805714+codewithritugautam@users.noreply.github.com>
2023-01-20 21:44:14 +0530

    Add files via upload

---
+++ Assignment.java
@@ -0,0 +1,14 @@
+class Assignment
+{
+public static void main(String...args)
+{
+int a=50;
+a+=3;
+System.out.println(a);
+a-=10;
+System.out.println(a);
+a*=2;
+System.out.println(a);
+a/=5;
+System.out.println(a);
+}}
