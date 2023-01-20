07c91d48e3f57b99cfef68dd0820727793e1ae69
codewithritugautam <122805714+codewithritugautam@users.noreply.github.com>
2023-01-20 21:38:52 +0530

    Add files via upload

---
+++ Addition.java
@@ -0,0 +1,11 @@
+class Addition
+{
+public static void main(String[]args)
+{
+System.out.println("Two two number is:");
+int a=5;
+int b=6;
+System.out.println(a + "   " +b);
+int sum=a+b;
+System.out.println("The sum is ="+sum);
+}}
