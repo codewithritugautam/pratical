4f2a344a9c96ea5946b9aca02cfb0c4454599053
codewithritugautam <122805714+codewithritugautam@users.noreply.github.com>
2023-01-20 21:45:35 +0530

    Add files via upload

---
+++ Logical.java
@@ -0,0 +1,15 @@
+class Logical
+{
+public static void main(String args[])
+{
+int a=2;
+int b=3;
+// AND &&
+System.out.println(a>b&&b<a);
+System.out.println(a<b&&b>a);
+System.out.println(a<b&&b>a);
+// OR ||
+System.out.println(a>b||b>a);
+System.out.println(a<b||b==a);
+System.out.println(a>b||b<a);
+}}
