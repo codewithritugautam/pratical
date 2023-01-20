c086d0c15d4d2fc78bd6c07bb8d08b9aaedd8ae7
codewithritugautam <122805714+codewithritugautam@users.noreply.github.com>
2023-01-20 21:54:35 +0530

    Add files via upload

---
+++ Unaryop.java
@@ -0,0 +1,19 @@
+class Unaryop
+{
+public static void main(String args[])
+{
+int a=4;
+int b=2;
+//postfix
+//System.out.println(a++);
+//System.out.println(++a);
+//prefix
+//System.out.println(a--);
+//System.out.println(--a);
+
+//System.out.println(a++ + ++b);
+//System.out.println(b++ + ++a);
+
+System.out.println(++b + ++b);
+System.out.println(a++ + ++a);
+}}
