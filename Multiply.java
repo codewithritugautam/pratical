7fe7b4be000c65838111784595b2825363b7a5d2
codewithritugautam <122805714+codewithritugautam@users.noreply.github.com>
2023-01-20 21:52:46 +0530

    Add files via upload

---
+++ Multiply.java
@@ -0,0 +1,14 @@
+class Multiply
+{
+public static void main(String args[])
+{
+int a=8;
+int b=5;
+int Multiply=a*b;
+System.out.println("first number is:");
+System.out.println("second number is:");
+System.out.println(a);
+System.out.println(b);
+System.out.println("the Multiply is="+Multiply);
+}}
+
