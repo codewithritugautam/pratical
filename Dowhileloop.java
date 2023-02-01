a5d18dac0398ccb11c65f8be90f6f1385701f73b
codewithritugautam <122805714+codewithritugautam@users.noreply.github.com>
2023-02-01 15:37:37 +0530

    Create Dowhileloop.java

---
+++ Dowhileloop.java
@@ -0,0 +1,15 @@
+package controlstatement;
+
+public class Dowhileloop {
+	public static void main(String[] args) {
+		int i=0;
+		System.out.println("First 5 even number \n");
+		do {
+			System.out.println(i); // statement
+			i=i+2;  // increment
+		}
+		while(i<=10); //condition
+		}
+	}
+
+
