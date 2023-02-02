7921c1052c9a9d731b640e9e65d1d0e0fc9eae37
codewithritugautam <122805714+codewithritugautam@users.noreply.github.com>
2023-02-02 15:43:47 +0530

    Create newpatten.java

---
+++ newpatten.java
@@ -0,0 +1,22 @@
+public class newpatten {
+	public static void main(String[] args) {
+		for( int i=5; i>=1; i--) //outer loop for row number
+		{
+			if(i % 2 !=0) { //odd number 1,3,5
+				for (int j =5 ; j>=i; j--) {  // odd position print star(*)
+					System.out.print(" * ");
+				}
+				System.out.println();
+			}
+		}
+		for(int i = 2; i<=5; i++)
+		{
+	if( i % 2 !=0) {
+		for( int j=5; j>=i; j--) {
+			System.out.print(" * ");
+		}
+		System.out.println();
+	}
+	    }
+		}
+	}
