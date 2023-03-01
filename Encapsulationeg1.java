4c723ef8abf6991d0481860e650936e02650502f
codewithritugautam <122805714+codewithritugautam@users.noreply.github.com>
2023-03-01 22:13:56 +0530

    Add files via upload

---
+++ Encapsulationeg1.java
@@ -0,0 +1,12 @@
+package Mypackage;
+
+public class Encapsulationeg1 {
+public static void main(String [] args) {
+	Encapsulation e=new Encapsulation();
+	e.setName("Ritu"); //setting value in the name
+	e.setAge(25); //setting value is the age
+	// getting value of the name and age
+	System.out.println("Name is:"+e.getName());
+	System.out.println("Age is:"+e.getAge());
+}
+}
