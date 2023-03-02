b236ee42b15a82411c785eac223c85b385f76605
codewithritugautam <122805714+codewithritugautam@users.noreply.github.com>
2023-03-02 22:32:17 +0530

    Add files via upload

---
+++ Arrayassignment.java
@@ -0,0 +1,11 @@
+package thurdylab;
+
+public class Arrayassignment {
+public static void main(String[] args) {
+	String[] name= {"ritu"," asha","sana","kiran","megha","nidhi","sumi","pihu","nisha","pari"};
+			for(int i=0;i<name.length;i++) {
+				System.out.println(name[i]);
+			}
+			System.out.println();
+}
+}
