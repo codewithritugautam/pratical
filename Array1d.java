f83da9cec52a2d9fbf307a9e3cfe009d0bb73ad7
codewithritugautam <122805714+codewithritugautam@users.noreply.github.com>
2023-03-02 22:37:35 +0530

    Add files via upload

---
+++ Array1d.java
@@ -0,0 +1,15 @@
+package thurdylab;
+//example of single dimensional array
+public class Array1d { 
+public static void main(String args[]) {
+	String [] names={" ritu"," asha","sana","kiran","megha","nidhi","sumi","pihu","nisha","pari"}; //declaring array
+	for(int i=0;i<names.length;i++) {
+		System.out.println(names[i]);
+	}
+	System.out.println();
+	int[] phone= {123,242,453,564,5465,245,464,234,222,444};
+	for(int j=0;j<phone.length;j++) {
+		System.out.println(phone[j]);
+	}
+}
+}
