3eca880e84f9da6f38f2c85cec2ebb836be09276
codewithritugautam <122805714+codewithritugautam@users.noreply.github.com>
2023-03-02 22:38:29 +0530

    Add files via upload

---
+++ Array3.java
@@ -0,0 +1,21 @@
+package thurdylab;
+
+public class Array3 {
+		public static void main(String args[]) {
+			int a[]=new int [5];//declaration & instantiation
+			//initialization
+			a[0]=2;
+			a[1]=9;
+			a[2]=4;
+			a[3]=7;
+			a[4]=6;
+			//traversing (printing one after another) an array using for loop
+			for(int i:a)
+				System.out.println(i);
+			//for(int i=0;i<a.length;i++) //length is one of the property of an array
+		 //System.out.println(a[i]);
+
+		}
+		}
+
+
