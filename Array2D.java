2c44a11e0aebe0bc0c8d45531d681eb2039431f9
codewithritugautam <122805714+codewithritugautam@users.noreply.github.com>
2023-03-06 22:27:32 +0530

    Add files via upload

---
+++ Array2D.java
@@ -0,0 +1,14 @@
+package controlstatement;
+//multidimensional Array
+public class Array2D {
+public static void main(String[] args) {
+	//declaration & initializing 2D array
+	int arr[] [] = {{2,5,6},{4,5,6}};
+	for(int i=0; i<2; i++) { //for printing row
+		for(int j=0;j<3;j++) {//for printing column
+			System.out.print(arr[i][j]+ " ");
+		}
+		System.out.println();
+	}
+}
+}
