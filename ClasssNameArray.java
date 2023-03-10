aacb604c43846d9d046aa352d3f65a6f571bb934
codewithritugautam <122805714+codewithritugautam@users.noreply.github.com>
2023-03-10 20:32:20 +0530

    Add files via upload

---
+++ ClasssNameArray.java
@@ -0,0 +1,13 @@
+package Assignment2;
+//to get the class name of array object in java
+public class ClasssNameArray {
+public static void main(String[] args) {
+	//declaration & initialization of an array
+	double arr[] = {1,2,3};
+	//getting the class name
+	Class a=arr.getClass();
+	String name= a.getName();
+	//print the class name
+	System.out.println(a);
+}
+}
