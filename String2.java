4fb7d961414d2c0e40c0b1da28e1dfdaa3c5d0d3
codewithritugautam <122805714+codewithritugautam@users.noreply.github.com>
2023-03-14 22:22:16 +0530

    Add files via upload

---
+++ String2.java
@@ -0,0 +1,12 @@
+package Assignment;
+
+public class String2 {
+	public static void mian(String[] args) {
+		String str ="Ritu";
+		str.concat("Gautam"); //insert the string at the end 
+		str=str.concat("Gautam"); //it will print Ritu gautam
+		System.out.println(str); //it will print Ritu only because string
+		}
+}
+
+
