fd0fc1ce0de5c3c9e1e85ba3626fc3c44205ea6f
codewithritugautam <122805714+codewithritugautam@users.noreply.github.com>
2023-03-15 21:14:56 +0530

    Add files via upload

---
+++ Switch3.java
@@ -0,0 +1,19 @@
+package controlstatement;
+
+public class Switch3 {
+public static void main(String[] args) {
+	char ch='r';
+    switch(ch)	{
+    case 'z':
+    	System.out.println("sana");
+    	break;
+    case 'p':
+    	System.out.println("ritu");
+    break;
+    case 'r':
+    	System.out.println("ritu gautam");
+    	break;
+    	default: System.out.println("Invalid");
+    }	
+}
+}
