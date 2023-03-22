ae19b6a6b4bae764351940931370b85f4c4246a9
codewithritugautam <122805714+codewithritugautam@users.noreply.github.com>
2023-03-22 16:18:31 +0530

    Add files via upload

---
+++ ThrowEg.java
@@ -0,0 +1,20 @@
+package Assignment;
+
+public class ThrowEg {
+//whether the student is pass or not
+	public static void mydisplay(int a) throws ArithmeticException{
+		if(a>=0 && a<35) {
+			//throw Arithmetic exception if not then fail
+			throw new ArithmeticException("you are fail");
+		}
+		else {
+			System.out.println("you are pass");
+		}}
+	//main method
+	public static void main(String[] args) {
+		//calling the function
+		mydisplay(13);
+		System.out.println("rest of the code...");
+		}
+	}
+
