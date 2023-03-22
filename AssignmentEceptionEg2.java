47bd2aa0e75e9606d53c87d5e01609939848c9b8
codewithritugautam <122805714+codewithritugautam@users.noreply.github.com>
2023-03-22 16:19:21 +0530

    Add files via upload

--- AssignmentEceptionEg2.java
+++ AssignmentEceptionEg2.java
@@ -1,50 +1,39 @@
 package Assignment;
 
 public class AssignmentEceptionEg2 {
-//division by zero
-	public void division() {
+	public static void main(String[] args) {
 		try {
 			int i=50/0;
 			System.out.println(i);
 		}
-		catch (Exception e) {
+		catch (ArithmeticException e) {
 	System.out.println(e);
-			System.out.println("Invalid division");
-		}}
-		//String parsed to a number variable 
-		public void StringParsed() {
+		}
 			try {
-				String b ="RituGautam";
-				int n = Integer.parseInt(b);
-				System.out.println(b); 
+				String s =null;
+				System.out.println(s.length()); 
+			}
+			catch(NullPointerException e) {
+				System.out.println(e);
+			}
+			try {
+				int arr[] = new int [5];
+				arr[7] = 10; 
+				System.out.println(arr);
+			}
+			catch (ArrayIndexOutOfBoundsException e) {
+				System.out.println(e);
+			}
+			try {
+				String s1 ="Ritu"; 
+				int n =Integer.parseInt(s1);
+				System.out.println(n);
 			}
 			catch(Exception e) {
 				System.out.println(e);
-				System.out.println("formet mismatch");
-			}}
-		//accessing an invalid calculator
-		public void calculator() {
-			try {
-				int a = 50/0+96+45/0; }
-			catch (Exception e) {
-				System.out.println("Calcution invalid");
-			}}
-		 //Accessing an invalid index in array : Print "Array index is invalid"
-		public void index() {
-			try {
-				int arr[] = new int [5];
-				arr[7] = 10; }
-			catch(Exception e) {
-				System.out.println(e);
-				System.out.println("Array index is invalid");
-			}}
-		//main method 
-		public static void main(String[] args ) {
-			System.out.println("Exception handling completed");
-			AssignmentEceptionEg2 obj = new AssignmentEceptionEg2();
-			obj.division();
-			obj.StringParsed();
-			obj.calculator();
-			obj.index();
+			}
+			finally {
+				System.out.println("Finally will be exceuted");
+			}
 			}
 		}
