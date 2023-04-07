355d08e5aca3c61aa0c78dde637a51a56202e446
codewithritugautam <122805714+codewithritugautam@users.noreply.github.com>
2023-04-07 17:21:28 +0530

    Add files via upload

---
+++ Simple_ArrayList_Eg.java
@@ -0,0 +1,16 @@
+package CollectionEg;
+import java.util.ArrayList;
+public class Simple_ArrayList_Eg {
+public static void main(String[] args) {
+	ArrayList<String> a1= new ArrayList<String>();
+	//add element in arraylist through object
+	a1.add("Ritu");
+	a1.add("upasana");
+	a1.add("megha");
+	a1.add("sana");
+	a1.add("hement");
+	a1.add("himanshu");
+	//printing arraylist
+	System.out.println(a1);
+}
+}
